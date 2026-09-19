a348301e640713bcb471eda0520d080a629939e4
James <98489957+coding-james@users.noreply.github.com>
2026-09-19 23:21:47 +0100

    Add additional columns and fields

--- codecademy/sql/01-films.sql
+++ codecademy/sql/01-films.sql
@@ -18,3 +18,23 @@ VALUES
 (9, 'Saving Private Ryan', 1998),
 (10, 'Star Wars: Episode IV - A New Hope', 1977),
 (11, 'WALL·E', 2008);
+
+-- Add new columns to the films table
+ALTER TABLE films
+ADD runtime INTEGER,
+ADD category TEXT,
+ADD rating TEXT,
+ADD box_office_earnings BIGINT;
+
+-- Update the new columns
+UPDATE films
+SET runtime = 136, category = 'Sci-Fi', rating = '15', box_office_earnings = 473330000
+WHERE name = 'The Matrix';
+
+UPDATE films
+SET runtime = 92, category = 'Comedy', rating = 'U', box_office_earnings = 579770000
+WHERE id = 2;
+
+UPDATE films
+SET runtime = 121, category = 'Sci-Fi', rating = 'U', box_office_earnings = 775398007
+WHERE name = 'Star Wars: Episode IV - A New Hope';
